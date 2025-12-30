849d9ee980257c025913fb956b14d78ae72b1ff1
demo_repo <raviharod7828@gmail.com>
2025-12-30 23:28:14 +0530

    fixing minor bugs

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -20,7 +20,7 @@ const sessionMiddleware = session({
   cookie: {
     httpOnly: true,
     secure: true,        // true if you serve over HTTPS
-    sameSite: none,      // 'lax' is fine for localhost; use 'none' + secure:true for cross-site
+    sameSite: 'none',      // 'lax' is fine for localhost; use 'none' + secure:true for cross-site
     maxAge: 1000 * 60 * 60 * 24, // 1 day
   },
 })
